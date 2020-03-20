91f1eee3d374e0693aba05eec5b1d9773ab199ef
unknown <orthogonaltrajectory@gmail.com>
2020-03-19 16:23:28 -0800

    GPS class template

---
+++ transmitter/gps.py
@@ -0,0 +1,17 @@
+"""
+gps.py
+Tristan Van Cise
+GPS class for retrieving coords from ultimate GPS v3
+03/19/2020
+
+Device: 
+    * Ultimate GPS Breakout V3
+"""
+
+import time
+import board
+import busio
+
+class GPS:
+    def __init__(self):
+        pass
