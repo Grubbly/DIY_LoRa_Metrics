cec0af048f428cfe1caa3914eebf6df146f35c97
unknown <orthogonaltrajectory@gmail.com>
2020-03-21 22:01:36 -0800

    Google maps prototype

---
+++ receiver/map-gui/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App";
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: ,
+    libraries: "places" // necessary for places input
+  }
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
